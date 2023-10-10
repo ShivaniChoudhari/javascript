3fce24fc3a359f614676005b6e3af61994d1e4b0
ShivaniChoudhari <91112250+ShivaniChoudhari@users.noreply.github.com>
2023-10-10 07:07:38 +0000

    const, let, var

---
+++ basics/const-var-let.js
@@ -0,0 +1,11 @@
+let accountName = "Shivanee";
+
+var accountLocation = "Pune";
+
+const accountId = 123456;
+
+accountEmail = "sc@gmail.com";
+
+let accountState;
+
+console.table([accountName, accountLocation, accountId, accountEmail, accountState]);
